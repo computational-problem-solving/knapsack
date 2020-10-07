caf6f04323cf3484fc4740a68b87bbaef89316c2
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-07 16:55:40 +0200

    :bug: fix(integerKnapsack): First draft.

--- src/integerKnapsack.js
+++ src/integerKnapsack.js
@@ -1,10 +1,18 @@
 import assert from 'assert';
 
-const integerKnapsack = (v, w, n, W) => {
+const integerKnapsack = (v, w, n, W, m = new w.constructor(W + 1).fill(0)) => {
 	assert(v.length === n);
 	assert(w.length === n);
 	assert(Number.isInteger(W) && W >= 0);
-	return 0;
+	assert(m.length >= W + 1);
+	for (let i = 0; i < n; ++i) {
+		const x = w[i];
+		for (let j = W; j >= x; --j) {
+			m[j] = Math.max(m[j], m[j - x] + v[i]);
+		}
+	}
+
+	return m[W];
 };
 
 export default integerKnapsack;

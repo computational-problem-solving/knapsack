b7b710701e0da9fe54507e7891f55b1ed8c1beb9
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-09 13:36:01 +0200

    :art: style(integerKnapsack): Use better variable names.

--- src/integerKnapsack.js
+++ src/integerKnapsack.js
@@ -6,12 +6,12 @@ const integerKnapsack = (v, w, n, W, m = new v.constructor(W + 1).fill(0)) => {
 	assert(Number.isInteger(W) && W >= 0);
 	assert(m.length >= W + 1);
 	for (let i = 0; i < n; ++i) {
-		const x = w[i];
-		const y = v[i];
-		assert(Number.isInteger(x) && x >= 0 && x <= W);
-		const s = W - x;
+		const wi = w[i];
+		const vi = v[i];
+		assert(Number.isInteger(wi) && wi >= 0 && wi <= W);
+		const s = W - wi;
 		for (let j = 0; j <= s; ++j) {
-			m[j] = Math.max(m[j], m[j + x] + y);
+			m[j] = Math.max(m[j], m[j + wi] + vi);
 		}
 	}
 

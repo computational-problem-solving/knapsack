bf34027e75c38ef1c6f712a3561264ee2cd2abd3
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-09 10:54:08 +0200

    :rocket: perf(integerKnapsack): Scan memory from left to right.

--- src/integerKnapsack.js
+++ src/integerKnapsack.js
@@ -7,12 +7,13 @@ const integerKnapsack = (v, w, n, W, m = new w.constructor(W + 1).fill(0)) => {
 	assert(m.length >= W + 1);
 	for (let i = 0; i < n; ++i) {
 		const x = w[i];
-		for (let j = W; j >= x; --j) {
-			m[j] = Math.max(m[j], m[j - x] + v[i]);
+		const s = W - x;
+		for (let j = 0; j <= s; ++j) {
+			m[j] = Math.max(m[j], m[j + x] + v[i]);
 		}
 	}
 
-	return m[W];
+	return m[0];
 };
 
 export default integerKnapsack;

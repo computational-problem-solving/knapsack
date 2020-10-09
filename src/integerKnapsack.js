da5a28e475608c04138c027605047623ac4ddb1b
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-09 10:56:07 +0200

    :rocket: perf(integerKnapsack): Cache v[i].

--- src/integerKnapsack.js
+++ src/integerKnapsack.js
@@ -7,10 +7,11 @@ const integerKnapsack = (v, w, n, W, m = new w.constructor(W + 1).fill(0)) => {
 	assert(m.length >= W + 1);
 	for (let i = 0; i < n; ++i) {
 		const x = w[i];
+		const y = v[i];
 		assert(Number.isInteger(x) && x >= 0 && x <= W);
 		const s = W - x;
 		for (let j = 0; j <= s; ++j) {
-			m[j] = Math.max(m[j], m[j + x] + v[i]);
+			m[j] = Math.max(m[j], m[j + x] + y);
 		}
 	}
 

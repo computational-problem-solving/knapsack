31670d313d64e1c9567d7d8025e9a792892d108e
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-10 22:29:17 +0200

    :sparkles: feat(knapsackApprox): First draft.

---
+++ src/knapsackApprox.js
@@ -0,0 +1,41 @@
+import assert from 'assert';
+import {increasing} from '@aureooms/js-compare';
+import {max, map, filter, range} from '@aureooms/js-itertools';
+
+import integerValuesKnapsack from './integerValuesKnapsack';
+
+/**
+ * (1-eps)-approx for the knapsack problem. Runs in O(N^3/eps) time.
+ *
+ * @param {Array} v Values.
+ * @param {Array} w Weights.
+ * @param {Array} n Size of the problem.
+ * @param {Array} W Size of the knapsack.
+ * @param {Array} eps Approximation constant.
+ * @param {Number} P Any lower bound on OPT > 0.
+ */
+const knapsackApprox = (
+	v,
+	w,
+	n,
+	W,
+	eps,
+	P = max(
+		increasing,
+		map(
+			(i) => v[i],
+			filter((i) => w[i] <= W, range(n)),
+		),
+	),
+) => {
+	assert(eps > 0 && eps < 1);
+	assert(P > 0);
+	assert(v.length === n);
+	assert(w.length === n);
+	const K = (eps * P) / n;
+	const u = Uint32Array.from(v, (vi) => Math.floor(vi / K));
+	// TODO reconstruct solution
+	return K * integerValuesKnapsack(u, w, n, W);
+};
+
+export default knapsackApprox;

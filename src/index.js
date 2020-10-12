72b3d0eb8bd6b349387e1976d9660cebd71cda5f
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-12 13:25:45 +0200

    :sparkles: feat: Export knapsackApprox.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import integerValuesKnapsack from './integerValuesKnapsack';
 import integerWeightsKnapsack from './integerWeightsKnapsack';
+import knapsackApprox from './knapsackApprox';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
 	integerValuesKnapsack,
 	integerWeightsKnapsack,
+	knapsackApprox,
 };
 
-export {integerValuesKnapsack, integerWeightsKnapsack};
+export {integerValuesKnapsack, integerWeightsKnapsack, knapsackApprox};
